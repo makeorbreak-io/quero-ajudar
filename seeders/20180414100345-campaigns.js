d388024d3b156e5617ead8b9303dd98965f67bb3
Jose Carneiro <1171385@isep.ipp.pt>
2018-04-14 18:16:06 +0100

    implement for

--- seeders/20180414100345-campaigns.js
+++ seeders/20180414100345-campaigns.js
@@ -1,52 +1,52 @@
 'use strict';
 module.exports = {
-    up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Campaigns', [
-    {
-        name: 'Juntos pelos  animais',
-		urlName: 'juntos-pelos-animais',
-        description: 'Associação de apoio e resgate dos animais, defesa contra os seus maus tratos e luta pela sua dignidade e direitos.',
-        category: '1',
-        website:'www.juntospelosanimais.com',
-        facebook:'facebook.com/juntospeloanimais',
-        instagram:'instagram.com/juntospelosanimais',
-        startingAt : new Date(),
-        endingAt : new Date(),
-        createdAt : new Date(),
-        updatedAt : new Date()
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.bulkInsert('Campaigns', [
+			{
+				name: 'Juntos pelos  animais',
+				urlName: 'juntos-pelos-animais',
+				description: 'Associação de apoio e resgate dos animais, defesa contra os seus maus tratos e luta pela sua dignidade e direitos.',
+				category: '1',
+				website:'www.juntospelosanimais.com',
+				facebook:'facebook.com/juntospeloanimais',
+				instagram:'instagram.com/juntospelosanimais',
+				startingAt : new Date(),
+				endingAt : new Date(),
+				createdAt : new Date(),
+				updatedAt : new Date()
 
 
 
-    },
-    {
-        name: 'Associação de Apoio à Vitima',
-		urlName: 'apav',
-        description: 'Apoiar as vítimas de crime, suas famílias e amigos, prestando-lhes serviços de qualidade, gratuitos e confidenciais e contribuir para o aperfeiçoamento das políticas públicas, sociais e privadas centradas no estatuto da vítima.',
-        category: '2',
-        website:'www.apav.pt',
-        facebook:'https://www.facebook.com/APAV.Portugal/',
-        startingAt : new Date(),
-        endingAt : new Date(),
-        createdAt : new Date(),
-        updatedAt : new Date()
+			},
+			{
+				name: 'Associação de Apoio à Vitima',
+				urlName: 'apav',
+				description: 'Apoiar as vítimas de crime, suas famílias e amigos, prestando-lhes serviços de qualidade, gratuitos e confidenciais e contribuir para o aperfeiçoamento das políticas públicas, sociais e privadas centradas no estatuto da vítima.',
+				category: '2',
+				website:'www.apav.pt',
+				facebook:'https://www.facebook.com/APAV.Portugal/',
+				startingAt : new Date(),
+				endingAt : new Date(),
+				createdAt : new Date(),
+				updatedAt : new Date()
 
 
-    },
-    {
-        name: 'Alcoolicos Anonimos',
-		urlName: 'alcoolicos-anonimos',
-        description: 'O nosso objetivo á ajudar alcoólicos a ultrapassarem o seu vicio, e dar o apoio necessário a ex-alc´´olico para que não tenham uma recaida',
-        category: '4',
-        website: 'www.alcoolicos-anonimos.pt',
-        startingAt : new Date(),
-        endingAt : new Date(),
-        createdAt : new Date(),
-        updatedAt : new Date()
-    }
+			},
+			{
+				name: 'Alcoolicos Anonimos',
+				urlName: 'alcoolicos-anonimos',
+				description: 'O nosso objetivo á ajudar alcoólicos a ultrapassarem o seu vicio, e dar o apoio necessário a ex-alc´´olico para que não tenham uma recaida',
+				category: '4',
+				website: 'www.alcoolicos-anonimos.pt',
+				startingAt : new Date(),
+				endingAt : new Date(),
+				createdAt : new Date(),
+				updatedAt : new Date()
+			}
 
-], {});
-},
+		], {});
+	},
 
-down: (queryInterface, Sequelize) => {
-}
+	down: (queryInterface, Sequelize) => {
+	}
 };
