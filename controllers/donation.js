2c7e93413911c5a9116b543b7c46a26e97ef139b
Jorge Vale <ajorgeav11@gmail.com>
2018-04-14 18:47:50 +0100

    np

--- controllers/donation.js
+++ controllers/donation.js
@@ -13,7 +13,7 @@ module.exports = {
 				.create({
 					amount: req.body.amount,
 					userId: req.session.userId,
-					organizationId: req.body.organizationId
+					organizationId: req.body.id
 				}).then(donation => {
 					next();
 				})
@@ -43,4 +43,39 @@ module.exports = {
 				});
 		}
 	},
+	donate: function(req, res, next){
+		const errors = validationResult(req);
+		console.log(errors);
+		if(!errors.isEmpty()){
+			res.status(400).json({errors: errors.mapped(), errorCode: 106}); //106 -> Error in input validation
+		} else {
+			Donation
+				.create({
+					amount: req.body.amount, userId: req.session.userId, category: req.body.category, location: req.body.location
+				})
+				.then(donation=>{
+					next();
+				})
+				.catch(err=>{
+					console.log(err);
+					res.status(500).json();
+				});
+		}
+	},
+	retrieveHistory: function(req, res, next){
+		Donation
+			.findOne({
+				where:{
+					userId: req.session.userId
+				}
+			})
+			.then(donations=>{
+				res.locals.donations = donations;
+				next();
+			})
+			.catch(err=>{
+				res.status(500).json();
+
+			});
+	}
 };
--- routes/middlewares/verifyAuth.js
+++ routes/middlewares/verifyAuth.js
@@ -1,11 +1,11 @@
 module.exports = {
 	user(req, res, next) {
 		if(req.session.userId){
+			console.log('BOOOOOOAS');
 			next();
 		} else {
 			res.status(401).redirect('/');
 		}
-
 	},
 	admin(req, res, next) {
 		if(req.session.isAdmin){
