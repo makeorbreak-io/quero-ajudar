74de1e6b50364fe9cec30231e220899abea8e5f0
Jorge Vale <ajorgeav11@gmail.com>
2018-04-14 13:23:49 +0100

    fixed modal state after close

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -3,4 +3,8 @@ $(document).ready(function(){
 		$('#loginModal').css('display','none');
 		$('#registerModal').css('display','block');
 	});
+	$('#authenticationModal').on('hide.bs.modal', function () {
+		$('#loginModal').css('display','block');
+		$('#registerModal').css('display','none');
+	});
 });
