c4a105f9f47b766dbd9625755beb3f7bee252cab
DoR18696 <36338524+DoR18696@users.noreply.github.com>
2018-04-15 00:40:52 +0100

    seeders headquarters

--- seeders/20180414212626-headquarters.js
+++ seeders/20180414212626-headquarters.js
@@ -1,39 +1,123 @@
 'use strict';
 
 module.exports = {
-	up: (queryInterface, Sequelize) => {
-		return queryInterface.bulkInsert('Headquarters', [
-			{
-				organizationId: 4,
-				name: 'Cáritas Diocesana Dos Açores',
-				lat: 38.6680878,
-				lng: -27.2484066,
-				address: 'Canada dos Folhadais, 54 – São Pedro',
-				zipCode: '9700-202 Andra do Heroísmo',
-				phoneNumber1: '295212795',
-				phoneNumber2:'',
-				email:'geral@caritasterceira.org',
-				location:'Açores',
-				createdAt : new Date(),
-				updatedAt : new Date()
-			},
-			{
-				organizationId: 4,
-				name: 'Cáritas Diocesana De Aveiro',
-				lat: 40.6450446,
-				lng: -8.6487256,
-				address: 'Rua do Carmo, nº 42',
-				zipCode: '3800-127 Aveiro',
-				phoneNumber1: '234 377 260',
-				phoneNumber2:'',
-				email:'aveiro@caritas.pt',
-				location:'Aveiro',
-				createdAt : new Date(),
-				updatedAt : new Date()
-			}
-		], {});
-	},
+    up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Headquarters', [
+        {
+            organizationId: 1,
+            name: 'Cáritas Diocesana Dos Açores',
+            lat: 38.6680878,
+            lng: -27.2484066,
+            address: 'Canada dos Folhadais, 54 – São Pedro',
+            zipCode: '9700-202 Andra do Heroísmo',
+            phoneNumber1: '295212795',
+            phoneNumber2:'',
+            email:'geral@caritasterceira.org',
+            location:'Açores',
+            createdAt : new Date(),
+            updatedAt : new Date()
+        },
+        {
+            organizationId: 2,
+            name: 'Cáritas Diocesana De Aveiro',
+            lat: 40.6450446,
+            lng: -8.6487256,
+            address: 'Rua do Carmo, nº 42',
+            zipCode: '3800-127 Aveiro',
+            phoneNumber1: '234 377 260',
+            phoneNumber2:'',
+            email:'aveiro@caritas.pt',
+            location:'Aveiro',
+            createdAt : new Date(),
+            updatedAt : new Date()
+        },
+        {
+            organizationId: 3,
+            name: 'Cáritas Diocesana De Aveiro',
+            lat: 40.6450446,
+            lng: -8.6487256,
+            address: 'Rua do Carmo, nº 42',
+            zipCode: '3800-127 Aveiro',
+            phoneNumber1: '234 377 260',
+            phoneNumber2:'',
+            email:'aveiro@caritas.pt',
+            location:'Aveiro',
+            createdAt : new Date(),
+            updatedAt : new Date()
+        },
+        {
+            organizationId: 4,
+            name: 'Cáritas Diocesana De Aveiro',
+            lat: 40.6450446,
+            lng: -8.6487256,
+            address: 'Rua do Carmo, nº 42',
+            zipCode: '3800-127 Aveiro',
+            phoneNumber1: '234 377 260',
+            phoneNumber2:'',
+            email:'aveiro@caritas.pt',
+            location:'Aveiro',
+            createdAt : new Date(),
+            updatedAt : new Date()
+        },
+        {
+            organizationId: 4,
+            name: 'Cáritas Diocesana Dos Açores',
+            lat: 38.6680878,
+            lng: -27.2484066,
+            address: 'Canada dos Folhadais, 54 – São Pedro',
+            zipCode: '9700-202 Andra do Heroísmo',
+            phoneNumber1: '295212795',
+            phoneNumber2:'',
+            email:'geral@caritasterceira.org',
+            location:'Açores',
+            createdAt : new Date(),
+            updatedAt : new Date()
+        },
+        {
+            campaignId: 1,
+            name: 'Cáritas Diocesana Dos Açores',
+            lat: 38.6680878,
+            lng: -27.2484066,
+            address: 'Canada dos Folhadais, 54 – São Pedro',
+            zipCode: '9700-202 Andra do Heroísmo',
+            phoneNumber1: '295212795',
+            phoneNumber2:'',
+            email:'geral@caritasterceira.org',
+            location:'Açores',
+            createdAt : new Date(),
+            updatedAt : new Date()
+        },
+        {
+            campaignId: 2,
+            name: 'Cáritas Diocesana De Aveiro',
+            lat: 40.6450446,
+            lng: -8.6487256,
+            address: 'Rua do Carmo, nº 42',
+            zipCode: '3800-127 Aveiro',
+            phoneNumber1: '234 377 260',
+            phoneNumber2:'',
+            email:'aveiro@caritas.pt',
+            location:'Aveiro',
+            createdAt : new Date(),
+            updatedAt : new Date()
+        },
+        {
+            campaignId: 3,
+            name: 'Cáritas Diocesana De Aveiro',
+            lat: 40.6450446,
+            lng: -8.6487256,
+            address: 'Rua do Carmo, nº 42',
+            zipCode: '3800-127 Aveiro',
+            phoneNumber1: '234 377 260',
+            phoneNumber2:'',
+            email:'aveiro@caritas.pt',
+            location:'Aveiro',
+            createdAt : new Date(),
+            updatedAt : new Date()
+        }
+    ], {});
+},
 
-	down: (queryInterface, Sequelize) => {
-	}
+down: (queryInterface, Sequelize) => {
+}
 };
