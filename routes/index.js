1a3d1b07b25bdbce7439b1b031b767ad48de0e5a
Jorge Vale <ajorgeav11@gmail.com>
2018-04-15 11:31:09 +0100

    ok

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('pages/index', { title: 'Express' , userLogged : req.session.userId, categories: config.categories, locations: config.location});
+	res.render('pages/index', { title: 'Express' , userLogged : req.session.userId, categories: config.categories, locations: config.locations});
 });
 
 module.exports = router;
