287da3a620cfcfa9a62227e17998fbcf12532d16
Jorge Vale <ajorgeav11@gmail.com>
2018-04-15 04:03:50 +0100

    ok

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,18 +1,15 @@
 $(document).ready(function(){
-
 	var goDonationAfterLogin = false;
-
+	var goDonateAfterLogin = false;
 	$('#registerBtn').click(function(){
 		$('#loginModal').css('display','none');
 		$('#registerModal').css('display','block');
 	});
-
 	$('#authenticationModal').on('hide.bs.modal', function () {
 
 		$('#loginModal').css('display','block');
 		$('#registerModal').css('display','none');
 	});
-
 	$('#doRegister').click(function(){
 		$.ajax({
 			type: 'POST',
@@ -29,7 +26,6 @@ $(document).ready(function(){
 				alert('ok');
 			});
 	});
-
 	$('#doLogin').click(function(){
 		$.post('/api/users/login', {email: 'diogoreis@gmail.com', password: 'Diogoreis18'})
 			.done(function () {
@@ -39,19 +35,19 @@ $(document).ready(function(){
 				alert('bad');
 			});
 	});
-
+	/*
+	DONATE TO RANDOM ORGANIZATION
+	 */
 	$('.goToQuantity').click(function () {
 		$('#quantityContainer').css('display','block');
 		$('#categoryContainer').css('display','none');
 		$('#locationContainer').css('display','none');
 	});
-
 	$('.goToCategory').click(function () {
 		$('#quantityContainer').css('display','none');
 		$('#categoryContainer').css('display','block');
 		$('#locationContainer').css('display','none');
 	});
-
 	$('.goToLocation').click(function () {
 		$('#quantityContainer').css('display','none');
 		$('#categoryContainer').css('display','none');
@@ -62,5 +58,27 @@ $(document).ready(function(){
 		$('#donationModal').modal('toggle');
 		$('#authenticationModal').modal('toggle');
 	});
-
+	/*
+	DONATE TO AN ORGANIZATION
+	 */
+	$('.goToAuthenticate1').click(function () {
+		goDonateAfterLogin = true;
+		$('#donateModal').modal('toggle');
+		$('#authenticationModal').modal('toggle');
+	});
+	$('.checkDonation').click(function () {
+		$('#checkDonationContainer').css('display','block');
+		$('#quantityContainer1').css('display','none');
+		$('#paymentContainer1').css('display','none');
+	});
+	$('.goToQuantity1').click(function () {
+		$('#checkDonationContainer').css('display','none');
+		$('#quantityContainer1').css('display','block');
+		$('#paymentContainer1').css('display','none');
+	});
+	$('.goToPayment').click(function () {
+		$('#checkDonationContainer').css('display','none');
+		$('#quantityContainer1').css('display','none');
+		$('#paymentContainer1').css('display','block');
+	});
 });
