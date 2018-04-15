4c19f49c175c1438226447899ff79db6acee2a73
Jorge Vale <ajorgeav11@gmail.com>
2018-04-15 15:31:01 +0100

    damn

--- seeders/20180415111304-partners.js
+++ seeders/20180415111304-partners.js
@@ -5,56 +5,56 @@ module.exports = {
     return queryInterface.bulkInsert('Partners', [
         {
             name: 'Porto',
-            logo: 'public/images/partners/portoLogo.svg',
-            website: 'www.porto.pt',
+            logo: '/images/partners/portoLogo.svg',
+            website: 'http://www.porto.pt',
             createdAt : new Date(),
             updatedAt : new Date()
         },
         {
             name: 'ScaleUp',
-            logo: 'public/images/partners/scaleUpLogo.svg',
-            website: 'www.scaleup.pt',
+            logo: '/images/partners/scaleUpLogo.svg',
+            website: 'http://www.scaleup.pt',
             createdAt : new Date(),
             updatedAt : new Date()
         },
         {
             name: 'Blip',
-            logo: 'public/images/partners/blipLogo.svg',
-            website: 'www.blip.pt',
+            logo: '/images/partners/blipLogo.svg',
+            website: 'http://www.blip.pt',
             createdAt : new Date(),
             updatedAt : new Date()
         },
         {
             name: 'Alientech',
-            logo: 'public/images/partners/alientechLogo.svg',
-            website: 'www.alientech.pt',
+            logo: '/images/partners/alientechLogo.svg',
+            website: 'http://www.alientech.pt',
             createdAt : new Date(),
             updatedAt : new Date()
         },
         {
             name: 'FEUP',
-            logo: 'public/images/partners/feupLogo.svg',
-            website: 'www.fe.up.pt',
+            logo: '/images/partners/feupLogo.svg',
+            website: 'http://www.fe.up.pt',
             createdAt : new Date(),
             updatedAt : new Date()
         },
         {
             name: 'Life On Mars',
-            logo: 'public/images/partners/lifeOnMarsLogo.svg',
+            logo: '/images/partners/lifeOnMarsLogo.svg',
             website: '',
             createdAt : new Date(),
             updatedAt : new Date()
         },
         {
             name: 'Abyssal',
-            logo: 'public/images/partners/abyssalLogo.svg',
+            logo: '/images/partners/abyssalLogo.svg',
             website: '',
             createdAt : new Date(),
             updatedAt : new Date()
         },
         {
             name: 'Hapibot',
-            logo: 'public/images/partners/hapibotLogo.svg',
+            logo: '/images/partners/hapibotLogo.svg',
             website: '',
             createdAt : new Date(),
             updatedAt : new Date()
